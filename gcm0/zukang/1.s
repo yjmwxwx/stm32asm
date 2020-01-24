97893e153936719487cc85cc1e7d77aa140e87e1
yjmwxwx <yjmwxwx@yandex.com>
2020-01-24 22:56:12 +0800

    定时器触发ADC

--- gcm0/zukang/1.s
+++ gcm0/zukang/1.s
@@ -1,11 +1,14 @@
 	@@单片机stm32f030f4p6
-	@@  pwm ne602时钟
+	@@ 高频阻抗测量
 	@作者：yjmwxwx
 	@时间：2019-06-30
 	@编译器：ARM-NONE-EABI
 	 .thumb
 	         .syntax unified
 	.section .data
+zheng_xian_biao:
+	.short 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+	.align 4
 shumaguanmabiao:
 	.int 0xfc,0x60,0xda,0xf2,0x66,0xb6,0xbe,0xe0,0xfe,0xf6
 shumaguanshuaxinbiao:
@@ -13,6 +16,9 @@ shumaguanshuaxinbiao:
 yjmwxwx:
 	.equ STACKINIT,        	        0x20001000
 	.equ shumaguanma,		0x20000000
+	.equ jishu2,			0x200000f8
+	.equ jishu,			0x200000fc
+	.equ dianyabiao,		0x20000100
 	.section .text
 vectors:
 	.word STACKINIT
@@ -30,7 +36,7 @@ vectors:
 	.word 0
 	.word 0
 	.word _pendsv_handler +1
-	.word aaa +1	@_systickzhongduan +1               @ 15
+	.word aaa +1	@ _systickzhongduan +1               @ 15
 	.word aaa +1     @ _wwdg +1          @ 0
 	.word aaa +1     @_pvd +1            @ 1
 	.word aaa +1     @_rtc +1            @ 2
@@ -47,7 +53,7 @@ vectors:
 	.word aaa +1       @_tim1_brk_up +1  @ 13
 	.word aaa +1        @ _tim1_cc +1    @ 14
 	.word aaa +1         @_tim2 +1       @ 15
-	.word aaa +1          @_tim3 +1      @ 16
+	.word _tim3 +1      @ 16
 	.word aaa +1                         @ 17
 	.word aaa +1		                @ 18
 	.word aaa +1	@_tim14 +1    @ 19
@@ -90,9 +96,18 @@ dengpll:
 	lsls r1, # 6
 	bpl dengpll
 
+	ldr r1, = 100
+	str r1, [r0, # 0x30]
 
-
-
+neicunqingling:
+	ldr r0, = 0x20000000
+	movs r1, # 0
+	ldr r3, = 0x1000
+neicunqinglingxunhuan:
+	subs r3, # 4
+	str r1, [r0, r3]
+	bne neicunqinglingxunhuan
+	
 _waisheshizhong:			 @ 外设时钟
 	@+0x14=RCC_AHBENR
 	@ 0=DMA @ 2=SRAM @ 4=FLITF@ 6=CRC @ 17=PA @ 18=PB @ 19=PC @ 20=PD @ 22=PF
@@ -102,13 +117,76 @@ _waisheshizhong:			 @ 外设时钟
 
 	@+0x18外设时钟使能寄存器 (RCC_APB2ENR)
 	@0=SYSCFG @5=USART6EN @9=ADC @11=TIM1 @12=SPI1 @14=USART1 @16=TIM15 @17=TIM16 @18=TIM17 @22=DBGMCU
-	ldr r1, = 0x800
+	ldr r1, = 0xa00
 	str r1, [r0, # 0x18]
 	@+0X1C=RCC_APB1ENR
 	@ 1=TIM3 @ 4=TIM6 @ 5=TIM7 @ 8=TIM14 @ 11=WWDG @ 14=SPI @ 17=USRT2 @ 18=USART3 @ 20=USART5 @ 21=I2C1
 	@ 22=I2C2 @ 23=USB @ 28=PWR
 	ldr r2, = 0x102
 	str r2, [r0, # 0x1c]
+
+_waishezhongduan:				@外设中断
+	@0xE000E100    0-31  写1开，写0没效
+	@0XE000E180    0-31 写1关，写0没效
+	@0XE000E200    0-31 挂起，写0没效
+	@0XE000E280    0-31 清除， 写0没效
+
+	ldr r0, = 0xe000e100
+	ldr r1, = 0x10000
+	str r1, [r0]
+_adcchushihua:
+        ldr r0, = 0x40012400  @ adc基地址
+        ldr r1, = 0x80000000
+        str r1, [r0, # 0x08]  @ ADC 控制寄存器 (ADC_CR)  @adc校准
+_dengadcjiaozhun:
+        ldr r1, [r0, # 0x08]
+        movs r1, r1
+        bmi _dengadcjiaozhun   @ 等ADC校准
+_kaiadc:
+        ldr r1, [r0, # 0x08]
+        movs r2, # 0x01
+        orrs r1, r1, r2
+        str r1, [r0, # 0x08]
+_dengdaiadcwending:
+        ldr r1, [r0]
+        lsls r1, r1, # 31
+        bpl _dengdaiadcwending @ 等ADC稳定
+_tongdaoxuanze:
+        ldr r1, = 0x80000000
+        str r1, [r0, # 0x10]
+        ldr r1, = 1
+        str r1, [r0, # 0x28]    @ 通道选择寄存器 (ADC_CHSELR)
+        ldr r1, = 0x8c3
+        str r1, [r0, # 0x0c]    @ 配置寄存器 1 (ADC_CFGR1)
+        movs r1, # 0
+        str r1, [r0, # 0x14]    @ ADC 采样时间寄存器 (ADC_SMPR)
+        ldr r1, [r0, # 0x08]
+        ldr r2, = 0x04         @ 开始转换
+        orrs r1, r1, r2
+        str r1, [r0, # 0x08]    @ 控制寄存器 (ADC_CR)
+
+	
+dmachushihua:
+        @+0=LSR,+4=IFCR,
+        @+8=CCR1,+c=CNDTR1,+10=CPAR1+14=CMAR1,
+        @+1c=CCR2,+20=CNDTR2,+24=CPAR2,+28=CMAR2
+        @+30=CCR3,+34=CNDTR3,+38=CPAR2,+3c=CMAR3
+        @+44=CCR4,+48=CNDTR4,+4c=CPAR4,+50=CMAR4
+        @+58=CCR5,+5c=CNDTR5,+60=CPAR5,+64=CMAR5
+        @+6C=CCR6,+70=CNDTR6,+74=CPAR6,+78=CMAR6
+        @+80=CCR7,+84=CNDTR7,+88=CPAR7,+8c=CMAR7
+
+	@ adc dma
+	ldr r0, = 0x40020000
+	ldr r1, = 0x40012440
+	str r1, [r0, # 0x10]
+	ldr r1, = dianyabiao
+	str r1, [r0, # 0x14]
+	ldr r1, =  2000
+	str r1, [r0, # 0x0c]
+	ldr r1, = 0x5a1		@ 5a1
+	str r1, [r0, # 0x08]
+
 
 tim1chushiha:				
 	ldr r5, = 0x40012c00 @ tim1_cr1
@@ -125,9 +203,34 @@ tim1chushiha:
 	ldr r1, = 1
 	str r1, [r5, # 0x3c]
 	movs r4, # 0xe1
-	str r4, [r5]
-	
 
+tim3chushihua:				
+	ldr r0, = 0x40000400 @ tim3_cr1
+	ldr r1, = 0
+	str r1, [r0, # 0x28] @ psc
+	ldr r1, = 27
+	str r1, [r0, # 0x2c] @ ARR
+	ldr r1, =  0x3800 @ 0x3800
+	str r1, [r0, # 0x1c] @ ccmr2
+	ldr r1, =  0x1000
+	str r1, [r0, # 0x20] @ ccer
+	ldr r1, = 27
+	str r1, [r0, # 0x40] @ ccr4
+	movs r1, # 0x70
+	str r1, [r0, # 0x04]
+	movs r1, # 0x01
+	str r1, [r0, # 0x0c]
+	movs r1, # 0xe1
+	str r1, [r0]
+	str r4, [r5]
+@_systick:				@ systick定时器初始化
+@
+@	ldr r0, = 0xe000e010
+@	ldr r1, = 4800000
+@	str r1, [r0, # 4]
+@	str r1, [r0, # 8]
+@	movs r1, # 0x07
+@	str r1, [r0]
 io_she_zhi:
 	@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 	@a(0x48000000)b(0x48000400)c(0x48000800)d(0x48000c00)f(0x48001400)
@@ -154,8 +257,13 @@ io_she_zhi:
 	str r1, [r0]
 	ldr r1, = 0x200
 	str r1, [r0, # 0x24]
+
+	
 ting:
-	ldr r0, = 12345
+	ldr r1, = jishu
+	ldr r0, = jishu2
+	ldr r0, [r0]
+	str r0, [r1]
 	movs r1, # 5
         ldr r2, = shumaguanma
 	movs r3, # 0xff		@小数点位置
@@ -284,6 +392,24 @@ _svc_handler:
 	bx lr
 _pendsv_handler:
 	bx lr
-_systickzhongduan:
 aaa:
+	bx lr
+_tim3:
+	ldr r0, = jishu2
+	ldr r1, [r0]
+	adds r1, r1, #1
+	str r1, [r0]
+_tim3fanhui:
+	ldr r0, = 0x40000400
+	movs r1, # 0
+	str r1, [r0, # 0x10]
+	bx lr
+_systickzhongduan:
+	ldr r2, = jishu
+	ldr r0, = 0xe0000d04
+	ldr r3, [r2]
+	ldr r1, = 0x02000000
+	adds r3, r3, # 1
+	str r3, [r2]
+	str r1, [r0]                 @ 清除SYSTICK中断
 	bx lr
